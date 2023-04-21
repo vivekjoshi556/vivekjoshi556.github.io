82995016dafa2257e9557111ff160827b387a77d
vivekjoshi556 <vivekjoshi556@gmail.com>
2023-04-20 22:08:19 -0700

    Go to dev Mode

--- src/components/projects/projects/MusicGenre.js
+++ src/components/projects/projects/MusicGenre.js
@@ -1,8 +1,6 @@
 const MusicGenre = () => {
     return (
-        <div className = "w-full">
-            <div className = "">MusicGenre</div>
-        </div>
+        <h2 className = "text-3xl font-bold glitch inline-block" data-text = "This&nbsp;Part&nbsp;is&nbsp;Under&nbsp;Construction.">This Part is Under Construction.</h2>
     );
 }
 
--- src/components/projects/projects/ServicePro.js
+++ src/components/projects/projects/ServicePro.js
@@ -1,8 +1,6 @@
 const ServicePro = () => {
     return (
-        <div className = "w-full">
-            <div className = "">ServicePro</div>
-        </div>
+        <h2 className = "text-3xl font-bold glitch inline-block" data-text = "This&nbsp;Part&nbsp;is&nbsp;Under&nbsp;Construction.">This Part is Under Construction.</h2>
     );
 }
 
